f478d679e56887aa3b9ecbab6f8fb8fc2d3eee10
p1utoze <aditya.n.awati@gmail.com>
2023-03-21 07:04:57 +0530

    Update API functionality

--- database.py
+++ database.py
@@ -11,31 +11,33 @@ def __get_connect():
      'port': 3306,
      'host': os.getenv('DATABASE_HOST')
      }
-    return mariadb.connect(**connect_params)
+    conn = mariadb.connect(**connect_params)
+    return conn
 
 
-# connect_params = __getenv()
-# print(connect_params.values())
-
-# conn = mariadb.connect(**connect_params)
-
-# db = conn.cursor()
-query = '''select p.location, count(*) as total_posts
-from post p
-# where location in ('karnataka')
-group by p.location
-having total_posts >=4
-order by total_posts desc;'''
-# try:
+# conn = __get_connect()
+# # print(connect_params.values())
 #
-#     db.execute(query)
-#     results = db.fetchall()
-#     for i in results:
-#         print(*i)
+#
+# db = conn.cursor()
+# query = '''select p.location, count(*) as total_posts
+# from post p
+# # where location in ('karnataka')
+# group by p.location
+# having total_posts >=4
+# order by total_posts desc;'''
+# id = 4
+# query2 = f'''select photo_id, video_id, user_id, caption, location from post where post_id={id}'''
+# try:
+#     db.execute(query2)
+#     results = db.fetchone()
+#     print(*results)
+#     # for i in results:
+#     #     print(*i)
 #    # Commit your changes in the database
-#     db.commit()
+#     conn.commit()
 # except:
 #    # Rollback in case there is any error
-#     db.rollback()
-#
-# db.close()
+#     conn.rollback()
+#     print('Error')
+# conn.close()
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from typing import Union
 import fastapi as fa
-from database import __getenv
+from database import __get_connect
 
 app = fa.FastAPI(title='Social_media_API')
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 @app.get("/posts/{post_id}")
-def get_postid(post_id: int, q: Union[str, None]=None):
-    connection_params = __getenv()
+async def get_postid(post_id: int, q: Union[str, None]=None):
+    conn = __get_connect()
+    db = conn.cursor()
+    query = f'''select photo_id, video_id, user_id, caption, location from post where post_id={post_id}'''
+    db.execute(query)
+    res = db.fetchone()
+    return dict(zip(('photo_id', 'video_id', 'user_id', 'caption', 'location'), res))
