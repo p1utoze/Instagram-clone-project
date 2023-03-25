8433a2417d13f7f31e5fa188aac2fee874c4528c
p1utoze <aditya.n.awati@gmail.com>
2023-03-25 18:37:43 +0530

    Update files

--- main.py
+++ main.py
@@ -16,15 +16,18 @@ async def get_postid(post_id: int, q: Union[str, None]=None):
     return dict(zip(('photo_id', 'video_id', 'user_id', 'caption', 'location'), res))
 
 
-@app.get("/users/")
-async def valid_user(user_id: int = -1, ):
-    if user_id == -1:
-        return "Invalid query!"
-    db.execute(f'''select EXISTS (select user_id from users WHERE user_id={user_id}) as user_exists;''')
-    user_exits = db.fetchone()
-    return {'user_exists': bool(user_exits)}
-
-@app.get()
-
-
-
+@app.get("/users")
+async def valid_user(user_id: int = -1, email: str = "XYZ"):
+    if user_id and user_id != -1:
+        db.execute(f'''SELECT user_id, username,
+                    IF(EXISTS(SELECT 1 FROM users WHERE user_id = 12), 'true', 'false') as user_exists
+                    FROM users
+                    WHERE user_id = 12;''')
+        user_exits = db.fetchone()
+        return {'user_exists': bool(user_exits)}
+    elif email:
+        db.execute(f'''select DISTINCT email, username, bio, profile_photo_url 
+                        from  users where email='{email}' group by email;''')
+        user_profile = db.fetchall()
+        return user_profile
+    return "Invalid query!"
