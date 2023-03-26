a15350dc79d07e9df6a4d2679cc60abf4648ffb2
p1utoze <aditya.n.awati@gmail.com>
2023-03-26 18:46:59 +0530

    Update files

--- static/script.js
+++ static/script.js
@@ -10,6 +10,7 @@ async function getLogin_redirect() {
   const password = document.getElementById('password').value;
 
   // Make the GET request to the API endpoint
+  if (verifyForm == true) {
   try {
     const response = await fetch(`http://127.0.0.1:8000/users?email=${email}`, {
         method:'GET',
@@ -20,15 +21,104 @@ async function getLogin_redirect() {
     if (data.user == true) {
         // Redirect to home.html if key is true
         window.location.href = 'index.html';
-      } else {
+      } 
+      else {
         // Display an error message if key is false
-        const errorMsg = document.getElementById('error-msg');
-        errorMsg.textContent = 'Incorrect email or password';
-        errorMsg.style.display = 'block';
+        const errorMsg = document.getElementById('error_message');
+        errorMsg.innerHTML = 'Incorrect email or password';
       }
-
   } catch (error) {
     console.log(error)
   }
-  });
+  }});
+}
+function changeImage() {
+  var images = document
+    .getElementById("slide-content")
+    .getElementsByTagName("img");
+
+  var i = 0;
+
+  for (i = 0; i < images.length; i++) {
+    var image = images[i];
+
+    if (image.classList.contains("active")) {
+      //remove active class from this image
+      image.classList.remove("active");
+
+      //if we are at the last image, then go back to the first image
+      if (i == images.length - 1) {
+        var nextImage = images[0];
+        nextImage.classList.add("active");
+        break;
+      }
+
+      var nextImage = images[i + 1];
+      nextImage.classList.add("active");
+      break;
+    }
+  }
+}
+
+function changeMode() {
+  var body = document.getElementsByTagName("body")[0];
+  var footerLinks = document
+    .getElementById("links")
+    .getElementsByTagName("a");
+
+  //if we are currently using dark mode
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+
+    for (let i = 0; i < footerLinks.length; i++) {
+      footerLinks[i].classList.remove("dark-mode-link");
+    }
+  } else {
+    //we are currently using the light
+    body.classList.add("dark");
+
+    for (let i = 0; i < footerLinks.length; i++) {
+      footerLinks[i].classList.add("dark-mode-link");
+    }
+  }
+}
+
+function verifyForm() {
+  var password = document.getElementById("password").value;
+  var error_message = document.getElementById("error_message");
+
+  if (password.length < 6) {
+    error_message.innerHTML = "Password is to short";
+    return false;
+  }
+
+  return true;
+}
+
+document.getElementById("dark-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  changeMode();
+});
+
+document.getElementById("login-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  verifyForm();
+});
+
+async function follower_stories(user_id) {
+  const imageContainer = document.getElementById('status-wrapper');
+
+  fetch(`https://api.example.com/followers/${user_id}`)
+  .then(response => response.json())
+  .then(data => {
+    // Get the list of img elements from the division container
+    const imgElements = imageContainer.querySelectorAll('img');
+    
+    // Loop through the img elements and set their src value based on the corresponding item in the response data list
+    imgElements.forEach((imgElement, index) => {
+      imgElement.src = data[index].imageUrl;
+    });
+  })
+  .catch(error => console.log(error));
 }
