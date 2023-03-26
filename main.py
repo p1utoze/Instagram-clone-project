a9683102d2eee5f464c4275b59ea010d49110322
p1utoze <aditya.n.awati@gmail.com>
2023-03-26 17:52:56 +0530

    Update api methods

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 import fastapi as fa
 from database import get_connected
+from random import sample
 from fastapi.middleware.cors import CORSMiddleware
 app = fa.FastAPI(title='Social_media_API')
 app.add_middleware(
@@ -37,10 +38,10 @@ async def valid_user(user_id: int = -1, email: str = "XYZ"):
         return {'user_exists': bool(user_exits)}
     elif isinstance(email, str):
         try:
-            db.execute(f'''select DISTINCT email, username, bio, profile_photo_url 
+            db.execute(f'''select DISTINCT user_id, email, username, bio, profile_photo_url 
                             from  users where email='{email}' group by email;''')
             user_profile = db.fetchone()
-            res = dict(zip(("email", "username", "bio", "profile_photo_url" ), user_profile))
+            res = dict(zip(("user_id", "email", "username", "bio", "profile_photo_url" ), user_profile))
             res['user'] = True
             db.close()
             return res
@@ -49,4 +50,22 @@ async def valid_user(user_id: int = -1, email: str = "XYZ"):
             return {'user': False}
     db.close()
     return "Invalid query!"
+
+@app.get("/followers/{user_id}")
+async def followers_transact(user_id: int):
+    db = await get_connected()
+    try:
+        db.execute(f'''select user_id, email, username, bio, profile_photo_url
+        # from users
+        # where user_id in (select follower_id from follows where followee_id  = {user_id});''')
+        resp = db.fetchall()
+        keys = ("user_id", "email", "username", "bio", "profile_photo_url")
+        response = {}
+        samples = sample(range(1, resp.__len__()+1), k=10)
+        for k, *v in zip(keys, zip(*[resp[i]for i in samples])):
+            response[k] = v
+        return response
+    except:
+        return 'Internal server error'
+
 
